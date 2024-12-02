9e91b20bf8cf902794d6f75a45911415c65dbd41
desireemezzotero <desireemezzotero1@gmail.com>
2024-12-02 17:38:53 +0100

    snack-3 corrrect

--- snack-3/script.js
+++ snack-3/script.js
@@ -3,7 +3,6 @@ const numbers = [2, 8, 4, 7, 2, 87];
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
 
-numbers.forEach( (e, i) => {
-  e = i+
-  console.log(e)
-})
+
+const NewArray = numbers.map(number => number + 1)
+console.log(NewArray)
